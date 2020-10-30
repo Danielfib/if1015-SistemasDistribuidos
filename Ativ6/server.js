58be55824b23b0bf48e25bcf1d680dac6dbda076
Daniel Filgueira <dfb2@cin.ufpe.br>
2020-10-29 23:00:22 -0300

    Implementing message feed sse viewer client

--- Ativ6/server.js
+++ Ativ6/server.js
@@ -1,11 +1,23 @@
 var WebSocketServer = require("ws").Server;
+var http = require("http");
+http.createServer(function (req, res) {
+    res.writeHeader(200, {
+      "Content-Type": "text/event-stream",
+      "Cache-Control": "no-cache",
+      Connection: "keep-alive",
+      "Access-Control-Allow-Origin": "*",
+    });
+    httpResource = res;
+}).listen(9090);
 
 wss = new WebSocketServer({ port: 8080, path: "/testing" });
 var clients = [];
+var httpResource;
 
 wss.on("connection", function (ws) {
     ws.on("message", function (message) {
         console.log("Msg received in server: %s ", message);
+        httpResource.write("data: " + message + "\n\n");
         BroadcastToOthers(ws, message);
     });
     
